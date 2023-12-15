13079d098ac7619eeaf9731abb48c5b2f2941da0
DCKAP <sathishksdckap@gmail.com>
2023-12-15 14:19:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let div = document.createElement('div')
+    div.innerHTML = '<div class="fulldiv"> <div class="indiv"><h2>Page Not Found </h2> <a id="p" onclick="turn()" href="./index.html">click  to go home page</a> <hr>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil laborum, ratione reiciendis, optio totam tenetur repellendus laudantium voluptate ad omnis dolores ipsa neque, dignissimos nam suscipit quisquam officia. Perferendis, asperiores!  </div></div>'
+
+
+  
+
+   
+
+
+
+    
+
+
